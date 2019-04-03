5997e9766c51f45e8216dfb89af90c26aef1e65a
github username <github email address>
2019-04-03 18:24:30 -0500

    done

---
+++ lib/dog.rb
@@ -0,0 +1,35 @@
+
+#require 'pry'
+#require 'rb-readline'
+
+class Dog
+    attr_accessor :name
+
+   @@all = []
+
+   def initialize(name)
+    @name = name 
+    @@all << self
+   end
+
+
+def self.clear_all
+    @@all.clear
+end
+
+def self.all
+    
+    @@all.each do |dog|
+        puts dog.name
+    end
+end    
+
+end
+
+
+# scott = Dog.new("scott")
+# bob = Dog.new("bob")
+# pooop = Dog.new("pooop")
+
+# binding.pry
+# puts "blah"
